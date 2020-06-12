0622816bb0963df2718f5514d4a12b2075c43c9f
Egor Bolgov <e.bolgov@semrush.com>
2020-06-13 01:19:47 +0300

    Initial commit

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    void OnTriggerEnter (Collider col)
+    {
+        Debug.Log("Collision: " + col.name);
+        if(col.name == "Cube")
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 1;
+    public float torque = 0.1f;
+    protected Rigidbody body;
+
+    protected virtual void OnEnable()
+    {
+        Debug.Log("Test");
+        body = GetComponent<Rigidbody>();
+        body.isKinematic = false;
+    }
+
+    protected virtual void OnDisable()
+    {
+        body.isKinematic = false;
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+    }
+
+    // Called once per time delta, this one is realtime processing
+    protected void FixedUpdate()
+    {
+        float turn = Input.GetAxis("Horizontal");
+        float speed = Input.GetAxis("Vertical");
+
+        body.AddForce(body.transform.forward * speed, ForceMode.VelocityChange);
+        body.AddRelativeTorque(Vector3.up * torque * turn);
+    }
+}
