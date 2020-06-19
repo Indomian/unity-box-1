bb1d97adb8d45487fd0a68564b975257d85a38ff
Egor Bolgov <e.bolgov@semrush.com>
2020-06-20 00:35:31 +0300

    Begin to implement Level 1, new doors

---
+++ Assets/Scripts/BigDoorController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigDoorController : Activable
+{
+    public GameObject door;
+    private Animator doorAnimator;
+    public bool opened {
+        get => _active;
+        set {
+            if (value) {
+                activate();
+            } else {
+                deactivate();
+            }
+        }
+    }
+
+    public override void activate() {
+        if (doorAnimator) {
+            doorAnimator.SetBool("isOpened", true);
+        }
+        base.activate();
+    }
+
+    public override void deactivate() {
+        if (doorAnimator) {
+            doorAnimator.SetBool("isOpened", false);
+        }
+        base.deactivate();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        opened = false;   
+        doorAnimator = door.GetComponent<Animator>();
+    }
+}
