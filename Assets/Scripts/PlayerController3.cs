c9aa7d74a2c2600422969c007bbe90fd4d1bfd0f
Egor Bolgov <e.bolgov@semrush.com>
2020-06-30 01:17:48 +0300

    Update player behaviour for more smooth results

--- Assets/Scripts/PlayerController3.cs
+++ Assets/Scripts/PlayerController3.cs
@@ -6,8 +6,10 @@ public class PlayerController3 : MonoBehaviour
 {
     public float speed = 3.0F;
     public float rotateSpeed = 3.0F;
-
     public float pushPower = 2.0f;
+    public float mass = 10.0f;
+
+    public float forceYCorrection = 0.1f;
 
     CharacterController controller;
 
@@ -27,10 +29,19 @@ public class PlayerController3 : MonoBehaviour
         controller.SimpleMove(forward * curSpeed);
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white, 2);
+        }
+    }
+
     // this script pushes all rigidbodies that the character touches
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
+        Debug.Log(hit.collider);
         Rigidbody body = hit.collider.attachedRigidbody;
 
         // no rigidbody
@@ -45,15 +56,29 @@ public class PlayerController3 : MonoBehaviour
             return;
         }
 
+        Vector3 yCorrection = new Vector3(0, forceYCorrection, 0);
+
+        Debug.DrawRay(hit.point, hit.moveDirection, Color.white, 2);
+        Debug.DrawRay(hit.point, hit.normal, Color.green, 2);
+
         // Calculate push direction from move direction,
         // we only push objects to the sides never up and down
         Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
 
+        Vector3 projectedPushDir = Vector3.Project(pushDir, -hit.normal);
+
+        Debug.DrawRay(hit.point + yCorrection, projectedPushDir, Color.blue, 2);
+        Debug.DrawRay(hit.point + yCorrection, pushDir, Color.red, 2);
+
         // If you know how fast your character is trying to move,
         // then you can also multiply the push velocity by that.
 
+
         // Apply the push
         //body.velocity = pushDir * pushPower;
-        body.AddForce(pushDir * pushPower, ForceMode.Force);
+        //body.AddForce(pushDir * mass * controller.velocity.magnitude, ForceMode.Impulse);
+        //body.AddForce(pushDir * pushPower, ForceMode.Force);
+        //body.AddForceAtPosition(pushDir * pushPower, hit.point + yCorrection, ForceMode.Force);
+        body.AddForceAtPosition(projectedPushDir * pushPower, hit.point + yCorrection, ForceMode.Force);
     }
 }
---
+++ Assets/Scripts/PlayerController4.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController4 : MonoBehaviour
+{
+    public float forwardForce = 1;
+    public float torque = 0.1f;
+
+    protected Rigidbody body;
+
+    protected virtual void OnEnable()
+    {
+        body = GetComponent<Rigidbody>();
+        body.isKinematic = false;
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        float turn = Input.GetAxis("Horizontal");
+        float speed = Input.GetAxis("Vertical");
+
+        Vector3 pushDir = new Vector3(body.transform.forward.x, 0, body.transform.forward.z);
+
+        body.AddRelativeForce(Vector3.forward * speed * forwardForce, ForceMode.Force);
+        body.AddRelativeTorque(Vector3.up * torque * turn);
+    }
+
+    // Called once per time delta, this one is realtime processing
+    protected void FixedUpdate()
+    {
+        
+    }
+}
